86322d8f3794d981be7627b5a636553a1b3e26c5
votruk <kurtovi@gmail.com>
2015-07-02 00:06:40 +0300

    White FAB

--- app/src/main/java/ru/kurtov/msttesttask/MainActivity.java
+++ app/src/main/java/ru/kurtov/msttesttask/MainActivity.java
@@ -7,8 +7,6 @@ import android.support.v4.view.ViewPager;
 
 public class MainActivity extends FragmentActivity {
 
-	TabLayout tabLayout;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +17,7 @@ public class MainActivity extends FragmentActivity {
 		viewPager.setAdapter(pagerAdapter);
 
 		TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
 		tabLayout.setTabsFromPagerAdapter(pagerAdapter);
 		tabLayout.setupWithViewPager(viewPager);
 
